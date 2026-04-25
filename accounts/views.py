1b00386371257bed9311256e24b768246a7d1835
achrid <juliosalabi@gmail.com>
2026-04-25 18:49:37 +0100

    fix home view

--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,33 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+
+def home(request):
+    return render(request, 'accounts/home.html')
+
 
 def register(request):
     if request.method == "POST":
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
 
-        User.objects.create_user(
-            username=username,
-            email=email,
-            password=password
-        )
+        User.objects.create_user(username=username, email=email, password=password)
 
         return redirect('login')
 
     return render(request, 'accounts/register.html')
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+
+    return render(request, 'accounts/login.html')
