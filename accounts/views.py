a5e1894d2ff359b3d72d1609a502b06b6276f6b8
achrid <juliosalabi@gmail.com>
2026-04-25 19:15:44 +0100

    fix installed apps

--- accounts/views.py
+++ accounts/views.py
@@ -1,33 +1,18 @@
-from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
-
-def home(request):
-    return render(request, 'accounts/home.html')
-
+from django.shortcuts import render, redirect
 
 def register(request):
     if request.method == "POST":
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        if User.objects.filter(username=username).exists():
+            return render(request, "register.html", {
+                "error": "Ce nom d'utilisateur existe déjà"
+            })
 
         User.objects.create_user(username=username, email=email, password=password)
+        return redirect("login")
 
-        return redirect('login')
-
-    return render(request, 'accounts/register.html')
-
-
-def login_view(request):
-    if request.method == "POST":
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None:
-            login(request, user)
-            return redirect('/')
-
-    return render(request, 'accounts/login.html')
+    return render(request, "register.html")
