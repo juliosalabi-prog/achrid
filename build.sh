e6664c313f9a0bd545da59aaa75cdd8297ce40af
achrid <juliosalabi@gmail.com>
2026-04-27 00:02:20 +0100

    auto create admin

--- build.sh
+++ build.sh
@@ -3,3 +3,16 @@
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py createsuperuser --noinput || true
+#!/usr/bin/env bash
+
+pip install -r requirements.txt
+python manage.py migrate
+
+# Créer automatiquement un admin
+python manage.py shell << END
+from django.contrib.auth.models import User
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@gmail.com", "12345678")
+END
+
+python manage.py collectstatic --noinput
