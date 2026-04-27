3a3ab1c68d88c27c7350982849b7f00b63ab06d1
achrid <juliosalabi@gmail.com>
2026-04-27 11:52:19 +0100

    fix wrong app name in urls

--- urls.py
+++ urls.py
@@ -1,28 +1,10 @@
-from app import views  # ou le bon nom d'app
-
-urlpatterns = [
-    path('', views.home, name='home'),
-]
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register/', views.register, name='register'),
-]
-
 from django.contrib import admin
 from django.urls import path
-from . import views
+from accounts import views   
 
 urlpatterns = [
-    path('', views.home, name='home'),  # 👈 IMPORTANT (page d’accueil)
     path('admin/', admin.site.urls),
-    path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
-]
-from django.urls import path
-from . import views
-
-urlpatterns = [
+    path('register/', views.register, name='register'),
     path('abonnes/', views.liste_abonnes, name='liste_abonnes'),
 ]
