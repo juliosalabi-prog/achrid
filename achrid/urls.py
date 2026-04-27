11afe70df4ad26b6bd9c00cf42cd5466833d9b00
achrid <juliosalabi@gmail.com>
2026-04-27 09:30:22 +0100

    ajout route abonnes

--- achrid/urls.py
+++ achrid/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from ton_app import views  # 👈 adapte le nom
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+
+    path('abonnes/', views.liste_abonnes, name='liste_abonnes'),  # 👈 AJOUT ICI
 ]
