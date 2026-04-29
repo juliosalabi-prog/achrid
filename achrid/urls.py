5906b93ad22fe9c52e36866d47cf0f4af1415887
achrid <juliosalabi@gmail.com>
2026-04-29 20:26:35 +0100

    fix admin route

--- achrid/urls.py
+++ achrid/urls.py
@@ -15,3 +15,15 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('abonnes/', views.liste_abonnes, name='liste_abonnes'),
 ]
+
+from django.contrib import admin
+from django.urls import path
+from comptes import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('login/', views.login_view, name='login'),
+    path('home/', views.home, name='home'),
+    path('abonnes/', views.liste_abonnes, name='liste_abonnes'),
+]
